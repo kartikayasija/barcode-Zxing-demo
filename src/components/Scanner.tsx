c1e873fb7a5d1950e4e2f1fd56e124bafd76a12e
Kartikay Asija <kartikay7057@gmail.com>
2023-08-14 04:16:10 +0530

    method changed to constraints

--- src/components/Scanner.tsx
+++ src/components/Scanner.tsx
@@ -1,35 +1,39 @@
 import { BrowserMultiFormatReader } from "@zxing/library";
-import { MouseEventHandler, useState } from "react";
+import { MouseEventHandler, useState,useRef } from "react";
 
 export const Scanner: React.FC = () => {
   const [result, setResult] = useState<string>();
   const reader = new BrowserMultiFormatReader();
+  const videoRef = useRef<HTMLVideoElement>(null)
   const startScanner: MouseEventHandler<HTMLButtonElement> = async (e) => {
     e.preventDefault();
-    const [deviceId] = await reader.listVideoInputDevices();
-    if (!deviceId) {
-      console.log("No Camera");
-    }
-    console.log(deviceId);
+    // const [deviceId] = await reader.listVideoInputDevices();
+    // if (!deviceId) {
+    //   console.log("No Camera");
+    // }
+    // console.log(deviceId);
 
-    const result = await reader.decodeOnceFromVideoDevice(
-      deviceId.deviceId,
-      "video"
-    );
-    if(result){
-      setResult(result.getText());
+    const constraints = {
+      video: {
+        facingMode: "environment",
+      },
+    };
+    if(videoRef.current){
+      const result = await reader.decodeOnceFromConstraints(constraints,videoRef.current);
+      if (result) {
+        setResult(result.getText());
+      }
     }
   };
   return (
     <>
       <button onClick={startScanner}>Start</button>
-      <button onClick={()=>reader.reset()}>Reset</button>
+      <button onClick={() => reader.reset()}>Reset</button>
 
-      <div style={{ display:'flex', justifyContent:'center',margin:'5rem 0' }}>
-        <video
-          id="video"
-          width="350"
-        ></video>
+      <div
+        style={{ display: "flex", justifyContent: "center", margin: "5rem 0" }}
+      >
+        <video ref={videoRef} id="video" width="350"></video>
       </div>
 
       {result && <h1>Result : {result}</h1>}
